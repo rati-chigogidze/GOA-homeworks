901b895e9a854f6f856e8d7fcbd312b60706dc8d
rati-chigogidze <ratichigo466@gmail.com>
2024-05-27 20:23:50 +0400

    third commit

---
+++ Level 07/homework/lesson 7.py
@@ -0,0 +1,288 @@
+#კონვერტაციის ფუნქციები.
+
+
+#მექანიკური კონვერტაცია
+ 
+
+print(type(str(15)))
+print(type(str(4.9)))
+print(type(str(100)))
+print(type(str(12 + 9.3)))
+print(type(str(223)))
+
+
+print(type(float(32)))
+print(type(float("7")))
+print(type(float(12)))
+print(type(float("9")))
+print(type(float("125")))
+
+
+print(type(int(5.7)))
+print(type(int(10)))
+print(type(int("2")))
+print(type(int("20")))
+print(type(int(73)))
+
+
+
+#ავტომატური კონვერტაცია
+
+
+
+print(8.3 + 12)
+print(1.5 * 7)
+print(23 / 4)
+print(40 % 7)
+print(120 // 39)
+
+
+
+
+
+
+#არითმეტიკული ოპერაციები მომხმარებლის მიერ შემოტანილ რიცხვებს შორის.
+
+numbers = (input("please enter 5 random numbers:"))
+print = numbers
+
+
+num1 = 19
+num2 = 3
+num4 = 7
+num3 = 36
+num5 = 25
+
+#num1
+
+print(num1 + num1)
+print(num1 + num2)
+print(num1 + num3)
+print(num1 + num4)
+print(num1 + num5)
+
+
+print(num1 - num1)
+print(num1 - num2)
+print(num1 - num3)
+print(num1 - num4)
+print(num1 - num5)
+ 
+
+print(num1 * num1)
+print(num1 * num2)
+print(num1 * num3)
+print(num1 * num4)
+print(num1 * num5)
+
+
+print(num1 / num1)
+print(num1 / num2)
+print(num1 / num3)
+print(num1 / num4)
+print(num1 / num5)
+
+
+print(num1 // num1)
+print(num1 // num2)
+print(num1 // num3)
+print(num1 // num4)
+print(num1 // num5)
+
+
+print(num1 % num1)
+print(num1 % num2)
+print(num1 % num3)
+print(num1 % num4)
+print(num1 % num5)
+
+
+#num
+
+
+print(num2 + num1)
+print(num2 + num2)
+print(num2 + num3)
+print(num2 + num4)
+print(num2 + num5)
+ 
+
+print(num2 - num1)
+print(num2 - num2)
+print(num2 - num3)
+print(num2 - num4)
+print(num2 - num5)
+ 
+
+print(num2 * num1)
+print(num2 * num2)
+print(num2 * num3)
+print(num2 * num4)
+print(num2 * num5)
+ 
+
+print(num2 / num1)
+print(num2 / num2)
+print(num2 / num3)
+print(num2 / num4)
+print(num2 / num5)
+ 
+
+print(num2 // num1)
+print(num2 // num2)
+print(num2 // num3)
+print(num2 // num4)
+print(num2 // num5)
+ 
+
+print(num2 % num1)
+print(num2 % num2)
+print(num2 % num3)
+print(num2 % num4)
+print(num2 % num5)
+ 
+
+#num3
+
+
+print(num3 + num1) 
+print(num3 + num2)
+print(num3 + num3)
+print(num3 + num4)
+print(num3 + num5)
+
+
+print(num3 - num1)
+print(num3 - num2)
+print(num3 - num3)
+print(num3 - num4)
+print(num3 - num5)
+
+
+print(num3 * num1)
+print(num3 * num2)
+print(num3 * num3)
+print(num3 * num4)
+print(num3 * num5)
+
+
+print(num3 / num1)
+print(num3 / num2)
+print(num3 / num3)
+print(num3 / num4)
+print(num3 / num5)
+
+
+print(num3 // num1)
+print(num3 // num2)
+print(num3 // num3)
+print(num3 // num4)
+print(num3 // num5)
+
+
+print(num3 % num1)
+print(num3 % num2)
+print(num3 % num3)
+print(num3 % num4)
+print(num3 % num5)
+
+
+#num4
+
+
+print(num4 + num1)
+print(num4 + num2)
+print(num4 + num3)
+print(num4 + num4)
+print(num4 + num5)
+
+
+print(num4 - num1)
+print(num4 - num2)
+print(num4 - num3)
+print(num4 - num4)
+print(num4 - num5)
+
+
+print(num4 * num1)
+print(num4 * num2)
+print(num4 * num3)
+print(num4 * num4)
+print(num4 * num5)
+
+
+print(num4 / num1)
+print(num4 / num2)
+print(num4 / num3)
+print(num4 / num4)
+print(num4 / num5)
+
+
+print(num4 // num1)
+print(num4 // num2)
+print(num4 // num3)
+print(num4 // num4)
+print(num4 // num5)
+
+
+print(num4 % num1)
+print(num4 % num2)
+print(num4 % num3)
+print(num4 % num4)
+print(num4 % num5)
+
+#num5
+
+
+print(num5 + num1)
+print(num5 + num2)
+print(num5 + num3)
+print(num5 + num4)
+print(num5 + num5)
+
+
+print(num5 - num1)
+print(num5 - num2)
+print(num5 - num3)
+print(num5 - num4)
+print(num5 - num5)
+
+
+print(num5 * num1)
+print(num5 * num2)
+print(num5 * num3)
+print(num5 * num4)
+print(num5 * num5)
+
+
+print(num5 / num1)
+print(num5 / num2)
+print(num5 / num3)
+print(num5 / num4)
+print(num5 / num5)
+
+
+print(num5 // num1)
+print(num5 // num2)
+print(num5 // num3)
+print(num5 // num4)
+print(num5 // num5)
+
+
+print(num5 % num1)
+print(num5 % num2)
+print(num5 % num3)
+print(num5 % num4)
+print(num5 % num5)
+
+
+
+
+ 
+# print(7 / 2) # ზუსტი გაყოფა /, 7 / 2 = 3.5
+# print(7 // 2) # არაზუსტი გაყოფა(რამდენჯერ მოთავსდება გასაყოფში გამყოფი), 5 // 2 = 2
+# print(7 % 2) # ნაშთიანი გაყოფა, 7 % 2 = 1 (მიბრუნებს ნაშთს)
+
+# int()_ს გადაეცემა ნებისმიერი მონაცემი (თუ მონაცემი არ არის ვალიდური ანუ სწორი) მაგ შემთხვევაში დამიბრუნდება error, ხოლო თუ სწორია მაშინ, int ფუნქცია კონკრეტულ მონაცემს გარდაქმნის მთელ რიცხვად
+# float()_ს გადაეცემა ნებისმიერი მონაცემი (თუ მონაცემი არ არის ვალიდური ანუ სწორი) მაგ შემთხვევაში დამიბრუნდება error, ხოლო თუ სწორია მაშინ, int ფუნქცია კონკრეტულ მონაცემს გარდაქმნის ათწილად რიცხვად
+# str()_ს გადაეცემა ნებისმიერი მონაცემს გარდაქმნის სტრინგად (ტექსტური მონაცემი "")
