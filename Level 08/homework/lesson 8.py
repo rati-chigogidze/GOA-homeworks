e8cbf3bd9b68d06df240c0374b670550f121f255
rati-chigogidze <ratichigo466@gmail.com>
2024-05-28 20:32:37 +0400

    fourth commit

---
+++ Level 08/homework/lesson 8.py
@@ -0,0 +1,484 @@
+#მაგალითები შედარების ორეპატორებზე
+
+num1 = 29
+num2 = 14
+num3 = 38
+num4 = 53
+num5 = 76
+num6 = 47
+
+
+
+
+#(>)
+
+#num1
+
+print(num1 > num1)
+print(num1 > num2)
+print(num1 > num3)
+print(num1 > num4)
+print(num1 > num5)
+print(num1 > num6)
+ 
+ 
+#num2
+
+
+print(num2 > num1)
+print(num2 > num2)
+print(num2 > num3)
+print(num2 > num4)
+print(num2 > num5)
+print(num2 > num6)
+
+
+#num3
+
+
+print(num3 > num1)
+print(num3 > num2)
+print(num3 > num3)
+print(num3 > num4)
+print(num3 > num5)
+print(num3 > num6)
+
+
+#num4 
+
+
+print(num4 > num1)
+print(num4 > num2)
+print(num4 > num3)
+print(num4 > num4)
+print(num4 > num5)
+print(num4 > num6)
+
+
+#num5
+
+
+print(num5 > num1)
+print(num5 > num2)
+print(num5 > num3)
+print(num5 > num4)
+print(num5 > num5)
+print(num5 > num6)
+
+
+#num6
+
+
+print(num6 > num1)
+print(num6 > num2)
+print(num6 > num3)
+print(num6 > num4)
+print(num6 > num5)
+print(num6 > num6)
+
+
+
+
+
+
+
+#(<)
+
+
+#num1
+
+print(num1 < num1)
+print(num1 < num2)
+print(num1 < num3)
+print(num1 < num4)
+print(num1 < num5)
+print(num1 < num6)
+ 
+ 
+#num2
+
+
+print(num2 < num1)
+print(num2 < num2)
+print(num2 < num3)
+print(num2 < num4)
+print(num2 < num5)
+print(num2 < num6)
+
+
+#num3
+
+
+print(num3 < num1)
+print(num3 < num2)
+print(num3 < num3)
+print(num3 < num4)
+print(num3 < num5)
+print(num3 < num6)
+
+
+#num4 
+
+
+print(num4 < num1)
+print(num4 < num2)
+print(num4 < num3)
+print(num4 < num4)
+print(num4 < num5)
+print(num4 < num6)
+
+
+#num5
+
+
+print(num5 < num1)
+print(num5 < num2)
+print(num5 < num3)
+print(num5 < num4)
+print(num5 < num5)
+print(num5 < num6)
+
+
+#num6
+
+
+print(num6 < num1)
+print(num6 < num2)
+print(num6 < num3)
+print(num6 < num4)
+print(num6 < num5)
+print(num6 < num6)
+
+
+
+
+
+
+
+#>=
+
+
+#num1
+
+print(num1 >= num1)
+print(num1 >= num2)
+print(num1 >= num3)
+print(num1 >= num4)
+print(num1 >= num5)
+print(num1 >= num6)
+ 
+ 
+#num2=
+
+
+print(num2 >= num1)
+print(num2 >= num2)
+print(num2 >= num3)
+print(num2 >= num4)
+print(num2 >= num5)
+print(num2 >= num6)
+
+
+#num3=
+
+
+print(num3 >= num1)
+print(num3 >= num2)
+print(num3 >= num3)
+print(num3 >= num4)
+print(num3 >= num5)
+print(num3 >= num6)
+
+
+#num4 =
+
+
+print(num4 >= num1)
+print(num4 >= num2)
+print(num4 >= num3)
+print(num4 >= num4)
+print(num4 >= num5)
+print(num4 >= num6)
+
+
+#num5=
+
+
+print(num5 >= num1)
+print(num5 >= num2)
+print(num5 >= num3)
+print(num5 >= num4)
+print(num5 >= num5)
+print(num5 >= num6)
+
+
+#num6=
+
+
+print(num6 >= num1)
+print(num6 >= num2)
+print(num6 >= num3)
+print(num6 >= num4)
+print(num6 >= num5)
+print(num6 >= num6)
+
+
+
+
+
+
+
+
+
+
+#<=
+
+
+#num1
+
+print(num1 <= num1)
+print(num1 <= num2)
+print(num1 <= num3)
+print(num1 <= num4)
+print(num1 <= num5)
+print(num1 <= num6)
+ 
+ 
+#num2
+
+
+print(num2 <= num1)
+print(num2 <= num2)
+print(num2 <= num3)
+print(num2 <= num4)
+print(num2 <= num5)
+print(num2 <= num6)
+
+
+#num3
+
+
+print(num3 <= num1)
+print(num3 <= num2)
+print(num3 <= num3)
+print(num3 <= num4)
+print(num3 <= num5)
+print(num3 <= num6)
+
+
+#num4 
+
+
+print(num4 <= num1)
+print(num4 <= num2)
+print(num4 <= num3)
+print(num4 <= num4)
+print(num4 <= num5)
+print(num4 <= num6)
+
+
+#num5
+
+
+print(num5 <= num1)
+print(num5 <= num2)
+print(num5 <= num3)
+print(num5 <= num4)
+print(num5 <= num5)
+print(num5 <= num6)
+
+
+#num6
+
+
+print(num6 <= num1)
+print(num6 <= num2)
+print(num6 <= num3)
+print(num6 <= num4)
+print(num6 <= num5)
+print(num6 <= num6)
+
+
+
+
+
+
+
+
+
+
+#(==)
+
+
+#num1
+
+print(num1 == num1)
+print(num1 == num2)
+print(num1 == num3)
+print(num1 == num4)
+print(num1 == num5)
+print(num1 == num6)
+ 
+ 
+#num2
+
+
+print(num2 == num1)
+print(num2 == num2)
+print(num2 == num3)
+print(num2 == num4)
+print(num2 == num5)
+print(num2 == num6)
+
+
+#num3
+
+
+print(num3 == num1)
+print(num3 == num2)
+print(num3 == num3)
+print(num3 == num4)
+print(num3 == num5)
+print(num3 == num6)
+
+
+#num4 
+
+
+print(num4 == num1)
+print(num4 == num2)
+print(num4 == num3)
+print(num4 == num4)
+print(num4 == num5)
+print(num4 == num6)
+
+
+#num5
+
+
+print(num5 == num1)
+print(num5 == num2)
+print(num5 == num3)
+print(num5 == num4)
+print(num5 == num5)
+print(num5 == num6)
+
+
+#num6
+
+
+print(num6 == num1)
+print(num6 == num2)
+print(num6 == num3)
+print(num6 == num4)
+print(num6 == num5)
+print(num6 == num6)
+
+
+
+
+
+
+
+
+
+
+#(=!)
+
+
+#num1
+
+print(num1 != num1)
+print(num1 != num2)
+print(num1 != num3)
+print(num1 != num4)
+print(num1 != num5)
+print(num1 != num6)
+ 
+ 
+#num2
+
+
+print(num2 != num1)
+print(num2 != num2)
+print(num2 != num3)
+print(num2 != num4)
+print(num2 != num5)
+print(num2 != num6)
+
+
+#num3
+
+
+print(num3 != num1)
+print(num3 != num2)
+print(num3 != num3)
+print(num3 != num4)
+print(num3 != num5)
+print(num3 != num6)
+
+
+#num4 
+
+
+print(num4 != num1)
+print(num4 != num2)
+print(num4 != num3)
+print(num4 != num4)
+print(num4 != num5)
+print(num4 != num6)
+
+
+#num5
+
+
+print(num5 != num1)
+print(num5 != num2)
+print(num5 != num3)
+print(num5 != num4)
+print(num5 != num5)
+print(num5 != num6)
+
+
+#num6
+
+
+print(num6 != num1)
+print(num6 != num2)
+print(num6 != num3)
+print(num6 != num4)
+print(num6 != num5)
+print(num6 != num6)
+
+
+
+
+#(and) ოპერატორი
+
+
+print(5 and 7)
+print(6 and 6)
+print(23 and 40)
+print(47 and 90)
+print(("8") and ("15"))
+print(5.8 and 62)
+print("117.3" and "84.7" )
+print(46 and 90)
+print(452 and 157)
+print(1230.6 and "1535")
+
+
+
+#(or) ოპერატორი
+
+
+print(5 or 7)
+print(6 or 6)
+print(23 or 40)
+print(47 or 90)
+print(("8") or ("15"))
+print(5.8 or 62)
+print("117.3" or "84.7" )
+print(46 or 90)
+print(452 or 157)
+print(1230.6 or "1535")
