a544a2691cb2893cb4fec8a389bfa9f7355a99e2
rati-chigogidze <ratichigo466@gmail.com>
2024-06-07 00:24:59 +0400

    seventh commit

---
+++ Level 10/homework/lesson 10.py
@@ -0,0 +1,79 @@
+for i in range(0, 21):
+    print(i)
+
+
+for i in range(1, 11):
+    print(i)
+
+
+for i in range(1, 101):
+    print(i)
+
+
+
+
+
+age = int(input(" please enter your age: "))
+print(age)
+
+if age == age > 18:
+    print("you are adult")
+
+else:
+    print("you are virgin")
+
+
+
+
+day = input("please enter random number 1/7:")
+print(day)
+
+if day == 1:
+    print("")
+
+else:
+    print("monday")
+
+
+if day == 2:
+    print("")
+
+else:
+    print("tuesday")
+
+
+if day == 3:
+    print("")
+
+else:
+    print("wednesday")
+
+
+if day == 4:
+    print("")
+
+else:
+    print("thursday")
+
+
+if day == 5:
+    print("")
+
+else:
+    print("friday")
+
+
+if day == 6:
+    print("")
+
+else:
+    print("saturday")
+
+
+if day == 7:
+    print("")
+
+else:
+    print("sunday")
+
+    
