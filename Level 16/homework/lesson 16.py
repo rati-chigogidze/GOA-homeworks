bdc83f4cd0fb8ab2d6e67ec9b5573bab9a9e6ceb
rati-chigogidze <ratichigo466@gmail.com>
2024-06-18 19:27:17 +0400

    tenth commit

---
+++ Level 16/homework/lesson 16.py
@@ -0,0 +1,9 @@
+
+fruits = ['banana', 'apple', 'cherry', 'peach', 'strawberies']
+veggies = ['cucumber', 'tomato', 'potato', 'salad', 'avocado']
+drinks = ['water', 'coca cola', 'tea', 'coffee', 'milk']
+cars = ['BMW', 'Mercedes', 'buggati', 'audi', 'volkswagen']
+cities = ['batumi', 'tbilisi', 'berlin', 'lisabon', 'ankara']
+
+
+print(fruits[len(fruits) -1])
