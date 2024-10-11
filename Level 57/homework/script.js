cbabe956a6a1c3189b75546d34c387540f607d0c
rati-chigogidze <ratichigo466@gmail.com>
2024-10-12 02:26:26 +0400

    commit

---
+++ Level 57/homework/script.js
@@ -0,0 +1,197 @@
+// 1.
+const greeting = "Hello, world!";
+console.log(greeting);
+
+// 2.
+let counter = 0;
+console.log(counter);
+
+// 3. 
+counter = 10;
+console.log(counter);
+
+// 4. 
+const birthYear = 1990; 
+console.log(birthYear);
+
+// 5. 
+let x = 5;
+let y = 10;
+console.log(x + y);
+
+// 6.
+const firstName = "John"; 
+console.log(firstName);
+
+// 7. 
+let temperature = 25;
+console.log(temperature);
+
+// 8. 
+const isAdult = true;
+console.log(isAdult);
+
+// 9. 
+let age = 30; 
+age = 31; 
+console.log(age);
+
+// 10. 
+const PI = 3.14159;
+console.log(PI);
+
+
+// 11. 
+let a = 5;
+let b = 3;
+console.log(a + b);
+
+// 12. 
+const multiplicationResult = 7 * 9;
+console.log(multiplicationResult);
+
+// 13. 
+let m = 20;
+let n = 4;
+console.log(m / n);
+
+// 14. 
+console.log(15 - 8);
+
+// 15. 
+console.log(10 % 4);
+
+// 16. 
+let num = 200;
+console.log(num + 100);
+
+// 17. 
+console.log((20 * 4) / 10);
+
+// 18. 
+let height = 5;
+let width = 10;
+console.log(height * width);
+
+// 19. 
+
+// 20. 
+console.log((100 - 50) / 2);
+
+
+// 21. 
+let x1 = 20;
+let y1 = 15;
+console.log(x1 > y1);
+
+// 22. 
+let a1 = 10;
+let b1 = '10';
+console.log(a1 === b1);
+
+// 23. 
+let age1 = 18;
+let age2 = 20;
+console.log(age1 < age2);
+
+// 24. 
+console.log(5 <= 10);
+
+// 25. 
+let p = 7;
+let q = 8;
+console.log(p !== q);
+
+// 26. 
+console.log(10 >= 10);
+
+// 27.
+let apple = "apple";
+let banana = "banana";
+console.log(apple == banana);
+
+// 28. 
+console.log(true !== false);
+
+// 29. 
+console.log(5 > 3);
+
+// 30. 
+console.log(8 < 12);
+
+
+// 31. 
+let myNumber = 45;
+console.log(typeof myNumber);
+
+// 32. 
+let result = 100 / 4;
+console.log(typeof result);
+
+// 33.
+console.log(typeof (5 + 7));
+
+// 34.
+let largeNumber = 1000000;
+console.log(largeNumber);
+
+// 35.
+let num1 = 20;
+let num2 = 5;
+console.log(num1 + num2);
+
+// 36. 
+console.log(50 - 30);
+
+// 37. 
+console.log(6 * 8);
+
+// 38. 
+let totalAmount = 500;
+console.log(totalAmount);
+
+// 39. 
+console.log(13 % 5);
+
+// 40. 
+console.log(typeof (9 / 3));
+
+
+// 41. 
+let isSunny = true;
+console.log(isSunny);
+
+// 42.
+let isRaining = false;
+console.log(!isRaining);
+
+// 43.
+let hasDriverLicense = true;
+let hasCar = false;
+console.log(typeof hasDriverLicense);
+console.log(typeof hasCar);
+
+// 44. 
+let isOpen = false;
+console.log(isOpen);
+
+// 45. 
+console.log(typeof (5 > 3));
+
+// 46. 
+let isActive = true;
+console.log(typeof isActive);
+
+// 47. 
+console.log(true !== false);
+
+// 48. 
+let isCompleted = false;
+console.log(isCompleted);
+
+// 49. 
+console.log(typeof false);
+
+// 50. 
+let isLoggedIn = true;
+console.log(isLoggedIn);
